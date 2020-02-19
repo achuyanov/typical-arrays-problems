20e1838942f1324526b9af58d71669c0a11abde8
AndreiChuyanov <achuyanov@gmail.com>
2020-02-19 22:03:41 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (arr) {
+  return validValue(arr) ? srt(arr).pop() : 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (arr) {
+  return validValue(arr) ? srt(arr)[0] : 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (arr) {
+  return validValue(arr) ? arr.reduce((sum, cur) => sum + cur) / arr.length : 0;
 }
+
+const srt = (arr) => arr.sort((a, b) => b - a);
+const validValue = (arr) => (arr && arr.length != 0);
